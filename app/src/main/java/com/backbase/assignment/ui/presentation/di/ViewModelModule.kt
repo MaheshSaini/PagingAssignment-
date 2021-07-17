517cdba8b29323a3cb0af3c4159429c667bc91f5
mahesh.kumar90394 <mahesh.kumar90394@homecredit.co.in>
2021-07-17 17:12:05 +0530

    TMDB assignment with paging,MVVM

---
+++ app/src/main/java/com/backbase/assignment/ui/presentation/di/ViewModelModule.kt
@@ -0,0 +1,11 @@
+package com.backbase.assignment.ui.presentation.di
+
+import com.backbase.assignment.ui.viewmodel.MainViewModel
+import com.backbase.assignment.ui.viewmodel.MovieBannerDetailsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { MainViewModel(get()) }
+    viewModel { MovieBannerDetailsViewModel(get()) }
+}
